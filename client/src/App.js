48fa1992c6ee803379d15f8e801f5399f636a2ad
eoengl <eoengl@naver.com>
2020-12-03 20:55:11 +0900

    temp save

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,13 +18,17 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 })
 
 class App extends Component {
 
   state = {
-    customers:""
+    customers:"",
+    complited: 0
   }
 
   componentDidMount() {
