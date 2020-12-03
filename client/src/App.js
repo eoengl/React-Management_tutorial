7ad1b2a2c28eea5cbe43e147ef654fca33b58b2b
eoengl <eoengl@naver.com>
2020-12-04 01:10:20 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -28,10 +28,11 @@ class App extends Component {
 
   state = {
     customers:"",
-    complited: 0
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -41,6 +42,11 @@ class App extends Component {
     const response = await fetch('/api/customers')
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed +1});
   }
 
   render() {
@@ -62,7 +68,13 @@ class App extends Component {
         {
           this.state.customers ? this.state.customers.map(c => {
             return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> );
-          }) : ""}
+          }) : 
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+            </TableCell>            
+          </TableRow>
+          }
            
         </TableBody>
         </Table>
