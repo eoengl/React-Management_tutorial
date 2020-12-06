6b31caec54f46d88e77c66643952286174f79996
eoengl <eoengl@naver.com>
2020-12-06 15:01:45 +0900

    Datebase

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port =process.env.PORT || 5000;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '홍길동',
-            'birthday' : '961222',
-            'gender' : '남자',
-            'job' : '대학생'
-        },
-        {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '루콩',
-            'birthday' : '930923',
-            'gender' : '여자',
-            'job' : '자영업자'
-        },
-        {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '노돈누',
-            'birthday' : '920502',
-            'gender' : '남자',
-            'job' : '요리사'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-      ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
