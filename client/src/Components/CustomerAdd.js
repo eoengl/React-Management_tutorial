669e9a605dba25ceb684c3a4ea2126b66b8624df
eoengl <eoengl@naver.com>
2020-12-07 03:12:55 +0900

    Finally I got it

--- client/src/Components/CustomerAdd.js
+++ client/src/Components/CustomerAdd.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {post} from 'axios';
+import { post } from 'axios';
 
 class CustomerAdd extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            file:null,
-            userName:'',
-            birthday:'',
-            gender:'',
-            job:'',
-            fileName:''
+            this.state = {
+                file:null,
+                userName:'',
+                birthday:'',
+                gender:'',
+                job:'',
+                fileName:''
         }
     }
 
     handleFormSubmit = (e) => {
-        e.proventDefault()
+        e.preventDefault()
         this.addCustomer()
             .then((response) => {
                 console.log(response.date);
@@ -30,15 +30,15 @@ class CustomerAdd extends React.Component {
         })
     }
 
-    handleValueChage = (e) => {
+    handleValueChange = (e) => {
         let nextState ={};
-        nextState{e.target.name} = e.target.value;
+        nextState[e.target.name] = e.target.value;
         this.setState(nextState);
     }
 
     addCustomer = () => {
         const url = '/api/customers';
-        const fromDate = new FormData();
+        const formData = new FormData();
         formData.append('image', this.state.file);
         formData.append('name', this.state.userName);
         formData.append('birthday', this.state.birthday);
@@ -57,11 +57,11 @@ class CustomerAdd extends React.Component {
         return (
             <form onSubmit={this.handleFormSubmit}>
                 <h1>고객추가</h1>
-                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChage={this.handleFileChange} /><br/>
+                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
                 이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValuChange} /><br/>
                 생년월일: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br/>
                 성별: <input type="text" name="gender" value={this.state.gender} onChange={this.hadleValueChage} /><br/>
-                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChage} /><br/>
+                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
                 <botton type="submit">추가하기</botton>
             </form>
         )
